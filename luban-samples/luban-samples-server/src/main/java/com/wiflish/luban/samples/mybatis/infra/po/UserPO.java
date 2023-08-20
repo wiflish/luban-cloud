11100b6459093d9d10a1e84d9888294e63b00edc
wiflish <wiflish.xie@gmail.com>
2023-08-20 21:12:26 +0800

    代码生成修改为h2数据库.

--- luban-samples/luban-samples-server/src/main/java/com/wiflish/luban/samples/mybatis/infra/po/UserPO.java
+++ luban-samples/luban-samples-server/src/main/java/com/wiflish/luban/samples/mybatis/infra/po/UserPO.java
@@ -1,22 +1,3 @@
-/*-
- * ************
- * luban-cloud
- * ------------
- * Copyright (C) 2023 - 2023 the original author or authors.
- * ------------
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ************
- */
 package com.wiflish.luban.samples.mybatis.infra.po;
 
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -24,9 +5,17 @@ import com.wiflish.luban.core.infra.po.BasePO;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author xiezhengrong
+ * @since 2023-08-20
+ */
 @Getter
 @Setter
-@TableName("t_user")
+@TableName("T_USER")
 public class UserPO extends BasePO {
 
     private static final long serialVersionUID = 1L;
