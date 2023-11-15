c21bf06177b98c8cf96919678b13562236a63757
wiflish <wiflish.xie@gmail.com>
2023-11-15 18:17:21 +0800

    SpringSecurity相关代码处理.

--- luban-starters/luban-web-starter/src/main/java/com/wiflish/luban/starter/web/config/LubanSecurityProperties.java
+++ luban-starters/luban-web-starter/src/main/java/com/wiflish/luban/starter/web/config/LubanSecurityProperties.java
@@ -3,12 +3,16 @@ package com.wiflish.luban.starter.web.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 /**
  * @author wiflish
  * @since 2023-09-21
  */
 @Data
 @ConfigurationProperties(prefix = "luban.security")
-public class SecurityProperties {
+public class LubanSecurityProperties {
     private Boolean disable = false;
+
+    private List<String> permitUrls;
 }
